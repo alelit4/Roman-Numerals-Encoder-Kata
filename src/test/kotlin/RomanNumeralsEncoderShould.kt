3c7636cd82219cffb804b25e85870b34778771dd
Alelit4 <alelit4@gmail.com>
2020-12-22 11:25:13 +0000

    Refactor throw_exception_when_receive_0_as_input

--- src/test/kotlin/RomanNumeralsEncoderShould.kt
+++ src/test/kotlin/RomanNumeralsEncoderShould.kt
@@ -6,8 +6,10 @@ internal class RomanNumeralsEncoderShould {
     @Test
     fun throw_exception_when_receive_0_as_input() {
         val romanNumeralsEncoder = RomanNumeralsEncoder()
-        Assertions.assertThrows(Exception::class.java) {
-            romanNumeralsEncoder.encode(0);
+        val exception = Assertions.assertThrows(Exception::class.java) {
+             romanNumeralsEncoder.encode(0)
         }
+        Assertions.assertEquals("No valid input", exception.message)
+
     }
 }
