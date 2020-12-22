54e968fb11af745a7ce735c94829be62691345ca
Alelit4 <alelit4@gmail.com>
2020-12-22 12:33:03 +0000

    GREEN: return_IV_when_receives_4_as_input

--- src/main/kotlin/RomanNumeralsEncoder.kt
+++ src/main/kotlin/RomanNumeralsEncoder.kt
@@ -1,11 +1,16 @@
 import java.lang.Exception
+import java.util.ArrayList
 
 class RomanNumeralsEncoder {
+    val romanBasicNumbers: HashMap<Int, String> = hashMapOf( 1 to "I", 4 to "IV", 5 to "V")
+
     fun encode(number: Int): String {
         if (number < 1) {
             throw Exception("No valid input")
         }
         var romanNumber = ""
+        if(number in romanBasicNumbers.keys)
+            return romanBasicNumbers[number].toString()
         for (element in 1..number){
             romanNumber += "I"
         }
