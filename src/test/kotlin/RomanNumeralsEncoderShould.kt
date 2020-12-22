95f25726f3fdffcdef1e337d9dfec951d3bfeb29
Alelit4 <alelit4@gmail.com>
2020-12-22 11:31:30 +0000

    RED: return_I_when_receives_1_as_input

--- src/test/kotlin/RomanNumeralsEncoderShould.kt
+++ src/test/kotlin/RomanNumeralsEncoderShould.kt
@@ -1,15 +1,23 @@
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
 internal class RomanNumeralsEncoderShould {
 
+    private val romanNumeralsEncoder = RomanNumeralsEncoder()
+
     @Test
-    fun throw_exception_when_receive_0_as_input() {
-        val romanNumeralsEncoder = RomanNumeralsEncoder()
+    fun throw_exception_when_receives_0_as_input() {
         val exception = Assertions.assertThrows(Exception::class.java) {
-             romanNumeralsEncoder.encode(0)
+            romanNumeralsEncoder.encode(0)
         }
         Assertions.assertEquals("No valid input", exception.message)
+    }
 
+    @Test
+    fun return_I_when_receives_1_as_input() {
+        val encodedNumber = romanNumeralsEncoder.encode(1)
+
+        Assertions.assertEquals("I", encodedNumber)
     }
 }
