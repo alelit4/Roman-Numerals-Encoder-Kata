a2567f3fc8ad3548c8a0550db596f5cb21c9b0ba
Alelit4 <alelit4@gmail.com>
2020-12-22 11:40:32 +0000

    GREEN: return_II_when_receives_2_as_input

--- src/main/kotlin/RomanNumeralsEncoder.kt
+++ src/main/kotlin/RomanNumeralsEncoder.kt
@@ -4,7 +4,9 @@ class RomanNumeralsEncoder {
     fun encode(number: Int): String {
         if (number < 1) {
             throw Exception("No valid input")
+        }else if(number == 1){
+            return "I"
         }
-        return "I"
+        return "II"
     }
 }
