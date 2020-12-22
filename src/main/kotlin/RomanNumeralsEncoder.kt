2325fd0ae7838317a1fc6f347f67bf741b820726
Alelit4 <alelit4@gmail.com>
2020-12-22 13:11:30 +0000

    GREEN: return_XL_when_receives_40_as_input

--- src/main/kotlin/RomanNumeralsEncoder.kt
+++ src/main/kotlin/RomanNumeralsEncoder.kt
@@ -2,7 +2,11 @@ import java.lang.Exception
 import java.util.ArrayList
 
 class RomanNumeralsEncoder {
-    private val romanBasicNumbers: HashMap<Int, String> = hashMapOf( 1 to "I", 4 to "IV", 5 to "V", 9 to "IX", 10 to "X")
+    private val romanBasicNumbers: HashMap<Int, String> = hashMapOf(
+        1 to "I", 4 to "IV", 5 to "V", 9 to "IX", 10 to "X",
+        40 to "XL", 50 to "L", 90 to "XC", 100 to "C", 400 to "CD", 500 to "D",
+        900 to "CM", 1000 to "M"
+    )
 
     fun encode(_number: Int): String {
         var number = _number
@@ -12,8 +16,9 @@ class RomanNumeralsEncoder {
         }
         var romanNumber = ""
 
-        for ( key in romanBasicNumbers.keys.sorted().reversed()){
-            while (number >= key ){
+        println(romanBasicNumbers.keys.sorted().reversed())
+        for (key in romanBasicNumbers.keys.sorted().reversed()) {
+            while (number >= key) {
                 romanNumber += romanBasicNumbers[key]
                 number -= key
             }
