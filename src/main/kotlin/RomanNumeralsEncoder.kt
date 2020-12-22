ccaf8fb10eea27d73b51b614c1d7922e494f1dd2
Ivan <Ivansantosgonz@gmail.com>
2020-12-22 12:46:09 +0000

    GREEN: return_VI_when_receives_6_as_input(){

--- src/main/kotlin/RomanNumeralsEncoder.kt
+++ src/main/kotlin/RomanNumeralsEncoder.kt
@@ -2,15 +2,24 @@ import java.lang.Exception
 import java.util.ArrayList
 
 class RomanNumeralsEncoder {
-    val romanBasicNumbers: HashMap<Int, String> = hashMapOf( 1 to "I", 4 to "IV", 5 to "V")
+    private val romanBasicNumbers: HashMap<Int, String> = hashMapOf( 1 to "I", 4 to "IV", 5 to "V")
 
-    fun encode(number: Int): String {
+    fun encode(_number: Int): String {
+        var number = _number
+
         if (number < 1) {
             throw Exception("No valid input")
         }
         var romanNumber = ""
         if(number in romanBasicNumbers.keys)
             return romanBasicNumbers[number].toString()
+
+        for ( key in romanBasicNumbers.keys.sorted().reversed()){
+            if (number > key ){
+                romanNumber += romanBasicNumbers[key]
+                number -= key
+            }
+        }
         for (element in 1..number){
             romanNumber += "I"
         }
